022fa0b0145a9c4a934bf455a89568f1f60510ab
aaondonn <aaondonn@gmail.com>
2025-08-20 21:35:14 -0500

    fixes, indent

--- main.py
+++ main.py
@@ -13,42 +13,42 @@ class TestUrbanRoutes:
             print("Cannot connect to Urban Routes. Check the server is on and still running")
 
 # Add in S8
-def test_set_route():
-    print("function created for set route")
-    pass
+    def test_set_route(self):
+        print("function created for set route")
+        pass
 
   # Add in S8
-def test_select_plan(self):
-    print()
-    pass
-
-  # Add in S8
-def test_fill_phone_number(self):
-    print()
-    pass
-
-   # Add in S8
-def test_fill_card(self):
-    print()
-    pass
-
-  # Add in S8
-def test_comment_for_driver(self):
-    print()
-    pass
-
- # Add in S8
-def test_order_blanket_and_handkerchiefs(self):
-    print()
-    pass
-
-# Add in S8
-def test_order_2_ice_creams(self):
-    for i in range(2):
+    def test_select_plan(self):
         print()
         pass
 
+  # Add in S8
+    def test_fill_phone_number(self):
+        print()
+        pass
+
+   # Add in S8
+    def test_fill_card(self):
+        print()
+        pass
+
+  # Add in S8
+    def test_comment_for_driver(self):
+        print()
+        pass
+
+ # Add in S8
+    def test_order_blanket_and_handkerchiefs(self):
+        print()
+        pass
+
+# Add in S8
+    def test_order_2_ice_creams(self):
+        for i in range(2):
+            print()
+            pass
+
     # Add in S8
-def test_car_search_model_appears(self):
-    print()
-    pass
+    def test_car_search_model_appears(self):
+        print()
+        pass
