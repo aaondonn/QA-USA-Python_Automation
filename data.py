80b595495c4b66aa6508fcf7b4f38485f3ce2965
aaondonn <aaondonn@gmail.com>
2025-09-19 18:39:06 -0500

    additional fixes, new code, I hate my life after working on this for so long. But it runs without errors now.

--- data.py
+++ data.py
@@ -1,8 +1,8 @@
-# File with constants
-URBAN_ROUTES_URL = ''
-ADDRESS_FROM =	'East 2nd Street, 601'
-ADDRESS_TO =	'1300 1st St'
-PHONE_NUMBER =	'+1 123 123 12 12'
-CARD_NUMBER =	'1234 5678 9100'
-CARD_CODE =	'1111'
-MESSAGE_FOR_DRIVER = 'Stop at the juice bar, please'
+# File with constants for the test suite
+BASE_URL = "https://cnt-521c4f29-694b-4d9b-9040-650960c4bc02.containerhub.tripleten-services.com/"
+FROM_ADDRESS = "East 2nd Street, 601"
+TO_ADDRESS = "1300 1st St"
+PHONE_NUMBER = "+1 123 123 1234"
+CARD_NUMBER = "1234 5678 9101 1121"
+CARD_CODE = "123"
+DRIVER_COMMENT = "Please call upon arrival."
--- pages.py
+++ pages.py
@@ -2,13 +2,14 @@ import time
 from selenium.common import TimeoutException
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.common.keys import Keys
 from locators import OrderPageLocators
-# We need our magical helper function!
 from helpers import retrieve_phone_code
 
 
 class BasePage:
     """Base Page class containing common methods for all pages."""
+
     def __init__(self, driver):
         self.driver = driver
 
@@ -30,84 +31,95 @@ class OrderPage(BasePage):
     locators = OrderPageLocators
 
     def set_route(self, from_address, to_address):
-        from_field = self.find_clickable_element(self.locators.FROM_FIELD)
-        from_field.clear()
-        from_field.send_keys(from_address)
-        to_field = self.find_clickable_element(self.locators.TO_FIELD)
-        to_field.clear()
-        to_field.send_keys(to_address)
+        self.find_element(self.locators.FROM_FIELD).send_keys(from_address)
+        self.find_element(self.locators.TO_FIELD).send_keys(to_address)
 
     def get_field_value(self, field_locator):
         return self.find_element(field_locator).get_attribute('value')
 
-    def click_call_taxi_button(self):
-        self.find_clickable_element(self.locators.CALL_TAXI_BUTTON).click()
-        time.sleep(1) # A brief pause for the tariff modal to appear
+    def click_order_button_initial(self):
+        self.find_clickable_element(self.locators.ORDER_BUTTON_INITIAL).click()
+        time.sleep(2)
 
     def select_supportive_tariff(self):
-        """
-        Selects the 'Supportive' plan only if it's not already selected.
-        This single command now handles both checking and clicking.
-        """
-        try:
-            # First, we check if the tariff is already active.
-            self.find_element(self.locators.ACTIVE_SUPPORTIVE_TARIFF, time=2)
-            # If the line above works, it means the tariff is already selected, so we do nothing!
-        except TimeoutException:
-            # If we get a TimeoutException, it means the tariff isn't active, so we click it.
-            self.find_clickable_element(self.locators.SUPPORTIVE_TARIFF).click()
+        self.find_clickable_element(self.locators.SUPPORTIVE_TARIFF).click()
+        self.find_element(self.locators.ACTIVE_TARIFF_TEXT)
 
-    def is_supportive_tariff_selected(self):
-        """
-        Checks if the 'Supportive' tariff plan is currently active.
-        This is now mainly used for assertions in our tests.
-        """
-        try:
-            self.find_element(self.locators.ACTIVE_SUPPORTIVE_TARIFF, time=2)
-            return True
-        except TimeoutException:
-            return False
+    def get_active_tariff_text(self):
+        """Retrieves the text of the currently active tariff plan."""
+        return self.find_element(self.locators.ACTIVE_TARIFF_TEXT, time=2).text
 
     def enter_phone_number_and_code(self, phone):
-        self.find_element(self.locators.PHONE_FIELD).send_keys(phone)
+        """Clicks the phone number button, enters the number, and confirms."""
+        # Step 1: Click the button to open the phone number entry modal
+        self.find_clickable_element(self.locators.PHONE_NUMBER_BUTTON).click()
+
+        # Step 2: Now find the input field and type the number
+        phone_input = self.find_clickable_element(self.locators.PHONE_FIELD)
+        phone_input.send_keys(phone)
+
+        # Step 3: Click 'Next'
         self.find_clickable_element(self.locators.PHONE_SUBMIT_BUTTON).click()
+        time.sleep(2)
+
+        # Step 4: Retrieve and enter the SMS code
         phone_code = retrieve_phone_code(self.driver)
-        code_field = self.find_element(self.locators.CODE_FIELD)
-        code_field.send_keys(phone_code)
+        self.find_element(self.locators.CODE_FIELD).send_keys(phone_code)
+
+        # Step 5: Click 'Confirm'
         self.find_clickable_element(self.locators.CONFIRM_CODE_BUTTON).click()
+        time.sleep(2)
+
+    def get_phone_number_value(self):
+        """Retrieves the value from the phone number input field."""
+        return self.get_field_value(self.locators.PHONE_FIELD)
 
     def add_payment_card(self, card_number, card_code):
+        """Adds a credit card after the pop-up appears."""
         self.find_clickable_element(self.locators.PAYMENT_METHOD_BUTTON).click()
+        time.sleep(2)
         self.find_clickable_element(self.locators.ADD_CARD_LINK).click()
-        self.find_element(self.locators.CARD_NUMBER_FIELD).send_keys(card_number)
+        time.sleep(2)
+
+        card_number_input = self.find_clickable_element(self.locators.CARD_NUMBER_FIELD)
+        card_number_input.send_keys(card_number)
+        time.sleep(2)
+
         card_code_field = self.find_element(self.locators.CARD_CODE_FIELD)
         card_code_field.send_keys(card_code)
-        self.find_clickable_element(self.locators.PAYMENT_MODAL).click()
+        time.sleep(2)
+        card_code_field.send_keys(Keys.TAB)
+
         self.find_clickable_element(self.locators.CARD_SUBMIT_BUTTON).click()
+        time.sleep(2)
         self.find_clickable_element(self.locators.CLOSE_PAYMENT_MODAL_BUTTON).click()
+        time.sleep(2)
+
+    def get_payment_method_text(self):
+        return self.find_element(self.locators.PAYMENT_METHOD_TEXT).text
 
     def set_comment_for_driver(self, comment):
         self.find_element(self.locators.COMMENT_FIELD).send_keys(comment)
 
     def request_blanket(self):
         self.find_clickable_element(self.locators.BLANKET_SWITCH).click()
+        time.sleep(2)
 
     def is_blanket_selected(self):
-        checkbox = self.find_element(self.locators.BLANKET_SWITCH_CHECKBOX)
-        return checkbox.is_selected()
+        return self.find_element(self.locators.BLANKET_SWITCH_CHECKBOX).is_selected()
 
     def add_ice_creams(self, number_of_ice_creams):
         plus_button = self.find_clickable_element(self.locators.ICE_CREAM_PLUS_BUTTON)
         for _ in range(number_of_ice_creams):
             plus_button.click()
+            time.sleep(2)
 
     def get_ice_cream_count(self):
         return int(self.find_element(self.locators.ICE_CREAM_COUNTER).text)
 
-    def click_order_button(self):
-        self.find_clickable_element(self.locators.ORDER_BUTTON).click()
+    def click_order_button_final(self):
+        self.find_clickable_element(self.locators.ORDER_BUTTON_FINAL).click()
+        time.sleep(2)
 
-    def get_car_search_modal_title(self):
-        modal_title_element = self.find_element(self.locators.CAR_SEARCH_MODAL_TITLE, time=15)
-        return modal_title_element.text
-
+    def wait_for_car_search_modal(self):
+        return self.find_element(self.locators.CAR_SEARCH_MODAL, time=15).is_displayed()
